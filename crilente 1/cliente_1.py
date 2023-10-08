602af5f54ba2f3c9dd9d353ec088a3976454cb87
JVictor011 <jvictor2021@alu.ufc.br>
2023-10-08 20:24:05 -0300

    tela e tratamento de busca

--- crilente 1/cliente_1.py
+++ crilente 1/cliente_1.py
@@ -1,30 +1,46 @@
+import tkinter as tk
 import socket
 
+def get_time():
+    cliente_socket.send("tempo".encode())
+    resposta = cliente_socket.recv(1024).decode()
+    result_label.config(text=f'Tempo do servidor: {resposta}')
 
-def menu(num):
-    if num == 0:
-        print("- Digite o nome do arquivo:")
-        print("- Digite 'tempo' para obter a hora do servidor.")
-        print("- Digite 'imagem' para receber uma imagem PNG.")
-        print("- Digite 'poema' para receber uma poema.")
-        print("- Digite 'listar' para listar os arquivos.")
-        print("- Digite 'exit' para sair.")
-
-def save_txt(resposta, mensagem):
-    with open(f'{mensagem}.txt', 'w') as file:
-        file.write(resposta)
-
-def listar_arquivos(cliente_socket):
-    cliente_socket.send("listar".encode())
-    lista_arquivos = cliente_socket.recv(1000000).decode()
-    print(f'Arquivos no diretório do servidor:\n{lista_arquivos}')
-
-def receber_imagem(cliente_socket):
+def receive_image():
+    filename = image_entry.get()  # Obtém o nome do arquivo da entrada de texto
+    cliente_socket.send(f"imagem:{filename}".encode())  # Envia a mensagem com o nome do arquivo
     img_size = int.from_bytes(cliente_socket.recv(4), byteorder='big')
     img_data = cliente_socket.recv(img_size)
-    with open('imagem_recebida.png', 'wb') as file:
-        file.write(img_data)
-    print("\n\nImagem recebida e salva como 'imagem_recebida.png'.\n\n")
+    resposta = b'ivo n\xc3\xa3o encontrado.'
+
+    if img_data == resposta:
+        result_label.config(text="Arq n encontrado!")
+    else:
+        with open(f'{filename}.png', 'wb') as file:
+            file.write(img_data)
+        result_label.config(text=f'Imagem recebida e salva como {filename}.png.')
+
+
+def receive_poem():
+    filename = poem_entry.get()
+    cliente_socket.send(f"poema:{filename}".encode())
+    resposta = cliente_socket.recv(10000000).decode()
+    if resposta != "tuacha" and resposta != "Arquivo nao encontrado.":
+        with open(f'{filename}.txt', 'w') as file:
+            file.write(resposta)
+        result_label.config(text=f"Poema recebido e salvo como '{filename}.txt'.")
+    else:
+        result_label.config(text=resposta)
+
+def list_files():
+    cliente_socket.send("listar".encode())
+    lista_arquivos = cliente_socket.recv(1000000).decode()
+    result_label.config(text=f'Arquivos no diretório do servidor:\n{lista_arquivos}')
+
+def exit_program():
+    cliente_socket.send("exit".encode())  # Envia uma mensagem de saída para o servidor
+    cliente_socket.close()
+    root.destroy()
 
 cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -35,29 +51,36 @@ except ConnectionError:
     print("Não foi possível conectar ao servidor.")
     exit()
 
-while True:
-    menu(0)
-    mensagem = str(input("> "))
+root = tk.Tk()
+root.title("Cliente de Servidor")
 
-    if mensagem == "exit":
-        break
-    elif mensagem == "tempo":
-        cliente_socket.send(mensagem.encode())
-        resposta = cliente_socket.recv(1024).decode()
-        print(f'Tempo do servidor: {resposta}')
-    elif mensagem == "imagem":
-        cliente_socket.send(mensagem.encode())
-        receber_imagem(cliente_socket)
-    elif mensagem == "lista":
-        listar_arquivos(cliente_socket)
-    else:
-        cliente_socket.send(mensagem.encode())
-        resposta = cliente_socket.recv(10000000).decode()
+menu_label = tk.Label(root, text="Selecione uma opção:")
+menu_label.pack()
 
-        if resposta != "tuacha":
-            save_txt(resposta, mensagem)
-            print("\n\nArquivo criado!\n\n")
-        else:
-            print(resposta)
+time_button = tk.Button(root, text="Obter Hora do Servidor", command=get_time)
+time_button.pack()
 
-print("Fim")
+image_label = tk.Label(root, text="Nome do arquivo de imagem:")
+image_label.pack()
+image_entry = tk.Entry(root)
+image_entry.pack()
+image_button = tk.Button(root, text="Receber Imagem PNG", command=receive_image)
+image_button.pack()
+
+poem_label = tk.Label(root, text="Nome do arquivo de texto")
+poem_label.pack()
+poem_entry = tk.Entry(root)
+poem_entry.pack()
+poem_button = tk.Button(root, text="Receber Poema", command=receive_poem)
+poem_button.pack()
+
+list_button = tk.Button(root, text="Listar Arquivos no Servidor", command=list_files)
+list_button.pack()
+
+exit_button = tk.Button(root, text="Sair", command=exit_program)
+exit_button.pack()
+
+result_label = tk.Label(root, text="")
+result_label.pack()
+
+root.mainloop()
